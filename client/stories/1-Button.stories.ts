bfd4ad933c45b5ab84af8c0a3250e1a5c6661318
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2019-12-25 00:45:06 +0900

    tsファイルでstories実装

--- client/stories/1-Button.stories.ts
+++ client/stories/1-Button.stories.ts
@@ -1,24 +1,18 @@
 import { action } from '@storybook/addon-actions'
-import { linkTo } from '@storybook/addon-links'
 
-import MyButton from './MyButton'
+import MyButton from './MyButton.vue'
 
 export default {
   title: 'Button'
 }
 
 export const text = () => ({
-  components: { MyButton },
+  components: {
+    MyButton
+  },
+
   template: '<my-button @click="action">Hello Button</my-button>',
   methods: { action: action('clicked') }
-})
-
-export const jsx = () => ({
-  components: { MyButton },
-  render() {
-    return <my-button onClick={this.action}>With JSX</my-button>
-  },
-  methods: { action: linkTo('clicked') }
 })
 
 export const emoji = () => ({
---
+++ client/stories/MyButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button :style="buttonStyles" @click="onClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: () => ({
+    buttonStyles: {
+      border: '1px solid #eee',
+      borderRadius: 3,
+      backgroundColor: '#FFFFFF',
+      cursor: 'pointer',
+      fontSize: 15,
+      padding: '3px 10px',
+      margin: 10
+    }
+  }),
+
+  methods: {
+    onClick() {
+      this.$emit('click')
+    }
+  }
+})
+</script>
