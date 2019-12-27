f579f3a2d3135e638e8cf804daa8006742a6384c
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2019-12-28 00:56:37 +0900

    storiesのts化

---
+++ storybook/stories/1-Button.stories.ts
@@ -0,0 +1,32 @@
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+
+import MyButton from './MyButton'
+
+export default {
+  title: 'Button'
+}
+
+export const text = () => ({
+  components: {
+    MyButton
+  },
+
+  template: '<my-button @click="action">Hello Button</my-button>',
+
+  methods: {
+    action: action('clicked')
+  }
+})
+
+export const emoji = () => ({
+  components: {
+    MyButton
+  },
+
+  template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
+
+  methods: {
+    action: action('clicked')
+  }
+})
