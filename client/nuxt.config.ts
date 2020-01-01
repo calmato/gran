8e8ee3cdc02b0a19c513c366c7969786a3bbfa3d
yamaday <e155417k@st.u-gakugei.ac.jp>
2020-01-02 01:54:39 +0900

    fix: dark: true => false
    基本色を明るめにする✨

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -34,7 +34,7 @@ const configuration: Configuration = {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
