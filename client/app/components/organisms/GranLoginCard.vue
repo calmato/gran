a7ecfbb6852ac1c6a008c7827ffecc8e553b890e
yamaday <e155417k@st.u-gakugei.ac.jp>
2020-01-03 21:17:04 +0900

    fix: リンクの修正とGranCardTextの適用

--- client/app/components/organisms/GranLoginCard.vue
+++ client/app/components/organisms/GranLoginCard.vue
@@ -1,32 +1,34 @@
 <template>
   <gran-card>
-    <div slot="contents">
+    <gran-card-text>
       <gran-login-form @login="login" />
       <div class="ma-2 text-center">
         <p>
-          <nuxt-link to="singup">新規登録する</nuxt-link>
+          <nuxt-link to="/singup">新規登録する</nuxt-link>
         </p>
         <p>
-          <nuxt-link to="forget">パスワードを忘れた</nuxt-link>
+          <nuxt-link to="/forget">パスワードを忘れた</nuxt-link>
         </p>
       </div>
       <v-divider class="mx-12 ma-4" />
       <gran-social-button name="google" @click="loginWithGoogle"
         >Googleでサインイン
       </gran-social-button>
-    </div>
+    </gran-card-text>
   </gran-card>
 </template>
 
 <script>
 import Vue from 'vue'
 import GranCard from '~/components/atoms/GranCard.vue'
+import GranCardText from '~/components/atoms/GranCardText.vue'
 import GranLoginForm from '~/components/molecules/GranLoginForm.vue'
 import GranSocialButton from '~/components/molecules/GranSocialButton.vue'
 
 export default Vue.extend({
   components: {
     GranCard,
+    GranCardText,
     GranLoginForm,
     GranSocialButton
   },
