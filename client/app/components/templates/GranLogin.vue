69cfadc92debb55c0f6a2371ee373e0d2453b1a5
yamaday <e155417k@st.u-gakugei.ac.jp>
2020-01-03 18:25:37 +0900

    feat: templatesコンポーネントの作成

---
+++ client/app/components/templates/GranLogin.vue
@@ -0,0 +1,14 @@
+<template>
+  <gran-login-card />
+</template>
+
+<script>
+import Vue from 'vue'
+import GranLoginCard from '~/components/organisms/GranLoginCard.vue'
+
+export default Vue.extend({
+  components: {
+    GranLoginCard
+  }
+})
+</script>
