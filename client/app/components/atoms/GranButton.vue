11ef955306a64a0116b32e6ecd14693bee805990
yamaday <e155417k@st.u-gakugei.ac.jp>
2020-01-03 17:55:45 +0900

    feat: atomsコンポーネントの実装
    とりあえずは使うプロパティだけを定義
    テストは後回し

---
+++ client/app/components/atoms/GranButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-btn :color="color" :block="block" :dark="dark" :outlined="outlined" @click="click">
+    <slot />
+  </v-btn>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    color: {
+      type: String,
+      default: 'primary'
+    },
+    block: {
+      type: Boolean,
+      default: false
+    },
+    dark: {
+      type: Boolean,
+      default: false
+    },
+    outlined: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    click() {
+      this.$emit('click')
+    }
+  }
+})
+</script>
---
+++ client/app/components/atoms/GranCard.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <slot name="contents" />
+    </v-card-text>
+    <slot />
+  </v-card>
+</template>
