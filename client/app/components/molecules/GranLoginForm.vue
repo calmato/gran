14dad94760d109701bd23be37dbffe5167c567a1
yamaday <e155417k@st.u-gakugei.ac.jp>
2020-01-04 18:08:56 +0900

    fix: ログインフォームをILoginFormを使ってdataバインド
    どこで最初にバインディングしようか悩んだがmoleculesにしておく

--- client/app/components/molecules/GranLoginForm.vue
+++ client/app/components/molecules/GranLoginForm.vue
@@ -1,7 +1,12 @@
 <template>
   <form>
-    <gran-text-field prepend-icon="email" label="Email..." />
-    <gran-text-field prepend-icon="lock" label="Password..." />
+    <gran-text-field v-model="loginForm.email" prepend-icon="email" label="Email..." />
+    <gran-text-field
+      v-model="loginForm.password"
+      prepend-icon="lock"
+      label="Password..."
+      type="password"
+    />
     <gran-button color="light-blue darken-1" block dark @click="click">ログイン</gran-button>
   </form>
 </template>
@@ -10,15 +15,22 @@
 import Vue from 'vue'
 import GranTextField from '~/components/atoms/GranTextField.vue'
 import GranButton from '~/components/atoms/GranButton.vue'
+import { ILoginForm } from '~/types/form'
 
 export default Vue.extend({
   components: {
     GranTextField,
     GranButton
   },
+  data: () => ({
+    loginForm: {
+      email: '',
+      password: ''
+    } as ILoginForm
+  }),
   methods: {
     click() {
-      this.$emit('login')
+      this.$emit('login', this.loginForm)
     }
   }
 })
