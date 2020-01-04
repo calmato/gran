e89f4ef7e57d37db3fff088424fcdce47eb6bb1f
yamaday <e155417k@st.u-gakugei.ac.jp>
2020-01-04 18:12:03 +0900

    feat: バインディングされた値を$emitで親コンポーネントに流す
    バインディングはmoleculesで終了してそれより上位はemitで流すことにした

--- client/app/components/templates/GranLogin.vue
+++ client/app/components/templates/GranLogin.vue
@@ -1,14 +1,20 @@
 <template>
-  <gran-login-card />
+  <gran-login-card @login="login" />
 </template>
 
-<script>
+<script lang="ts">
 import Vue from 'vue'
 import GranLoginCard from '~/components/organisms/GranLoginCard.vue'
+import { ILoginForm } from '~/types/form/'
 
 export default Vue.extend({
   components: {
     GranLoginCard
+  },
+  methods: {
+    login(loginForm: ILoginForm) {
+      this.$emit('login', loginForm)
+    }
   }
 })
 </script>
--- client/app/pages/signin.vue
+++ client/app/pages/signin.vue
@@ -1,15 +1,22 @@
 <template>
-  <gran-login />
+  <gran-login @login="login" />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import GranLogin from '~/components/templates/GranLogin.vue'
+import { ILoginForm } from '~/types/form'
 
 export default Vue.extend({
   components: {
     GranLogin
   },
-  layout: 'auth'
+  layout: 'auth',
+  methods: {
+    login(loginForm: ILoginForm) {
+      // ここでvuexと通信する
+      console.log(loginForm)
+    }
+  }
 })
 </script>
