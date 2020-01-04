c19c879bcf3ea1cf3ddbb7ad323315b62959aaaf
yamaday <e155417k@st.u-gakugei.ac.jp>
2020-01-05 00:01:35 +0900

    feat: firebaseの設定を記述
    環境変数の読み取りがうまくいかなかったのでnuxt.config.tsを経由している

---
+++ client/app/plugins/firebase.ts
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
+  })
+}
+
+export default firebase
--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -48,6 +48,14 @@ const configuration: Configuration = {
         }
       }
     }
+  },
+  env: {
+    API_KEY: process.env.API_KEY!,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN!,
+    DATABASE_URL: process.env.DATABASE_URL!,
+    PROJECT_ID: process.env.PROJECT_ID!,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET!,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID!
   }
 }
 
