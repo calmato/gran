7bcc7c9e04c76359ccb88f1290d2243a86fb88e4
yamaday <e155417k@st.u-gakugei.ac.jp>
2020-01-05 18:20:58 +0900

    feat: LoginFormValidateObjectの実装
    やや冗長かもしれないが，インターフェースを定義してvalidateを実装してexportした

--- client/app/components/molecules/GranLoginForm.vue
+++ client/app/components/molecules/GranLoginForm.vue
@@ -4,14 +4,14 @@
       v-model="loginForm.email"
       prepend-icon="email"
       label="Email"
-      rules="required|email"
+      :rules="loginFormValidate.email"
     />
     <gran-text-field
       v-model="loginForm.password"
       prepend-icon="lock"
       label="Password"
       type="password"
-      rules="required|alpha_dash"
+      :rules="loginFormValidate.password"
     />
     <gran-button color="light-blue darken-1" block dark @click="click">ログイン</gran-button>
   </form>
@@ -21,7 +21,7 @@
 import Vue from 'vue'
 import GranTextField from '~/components/atoms/GranTextField.vue'
 import GranButton from '~/components/atoms/GranButton.vue'
-import { ILoginForm } from '~/types/form'
+import { ILoginForm, LoginFormValidate } from '~/types/form'
 
 export default Vue.extend({
   components: {
@@ -32,7 +32,8 @@ export default Vue.extend({
     loginForm: {
       email: '',
       password: ''
-    } as ILoginForm
+    } as ILoginForm,
+    loginFormValidate: LoginFormValidate
   }),
   methods: {
     click() {
--- client/app/types/form/auth/login.ts
+++ client/app/types/form/auth/login.ts
@@ -2,3 +2,18 @@ export interface ILoginForm {
   email: string
   password: string
 }
+
+interface ILoginFormValidate {
+  email: Object
+  password: Object
+}
+
+export const LoginFormValidate: ILoginFormValidate = {
+  email: {
+    required: true,
+    email: true
+  },
+  password: {
+    required: true
+  }
+}
