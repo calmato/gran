893c8cae91564b16a364f8d7603e65414ab85177
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-01-06 03:28:38 +0900

    feat: Validatorの作成

--- api/user/internal/application/validation/base.go
+++ api/user/internal/application/validation/base.go
@@ -1,47 +1,29 @@
 package validation
 
 import (
-	"regexp"
-
 	"github.com/go-playground/validator/v10"
 )
 
-const (
-	passwordString = "^[a-zA-Z0-9_!@#$_%^&*.?()-=+]*$"
-)
-
-var (
-	passwordRegex = regexp.MustCompile(passwordString)
-)
-
-// CustomValidator - カスタムバリデーションインターフェース
-type CustomValidator interface {
+// FormValidator - リクエストフォームバリデーションインターフェース
+type FormValidator interface {
 	Run(i interface{}) error
 }
 
-// customValidator - バリデーション用の構造体
-type customValidator struct {
+// formValidator - バリデーション用の構造体
+type formValidator struct {
 	validate *validator.Validate
 }
 
-// NewCustomValidator - Validatorの生成
-func NewCustomValidator() CustomValidator {
+// NewFormValidator - Validatorの生成
+func NewFormValidator() FormValidator {
 	validate := validator.New()
 
-	if err := validate.RegisterValidation("password", passwordCheck); err != nil {
-		return nil
-	}
-
-	return &customValidator{
+	return &formValidator{
 		validate: validate,
 	}
 }
 
 // Run - バリデーションの実行
-func (cv *customValidator) Run(i interface{}) error {
-	return cv.validate.Struct(i)
-}
-
-func passwordCheck(fl validator.FieldLevel) bool {
-	return passwordRegex.MatchString(fl.Field().String())
+func (fv *formValidator) Run(i interface{}) error {
+	return fv.validate.Struct(i)
 }
---
+++ api/user/internal/infrastructure/validation/base.go
@@ -0,0 +1,47 @@
+package validation
+
+import (
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+)
+
+const (
+	passwordString = "^[a-zA-Z0-9_!@#$_%^&*.?()-=+]*$"
+)
+
+var (
+	passwordRegex = regexp.MustCompile(passwordString)
+)
+
+// DomainValidator - ドメインバリデーションインターフェース
+type DomainValidator interface {
+	Run(i interface{}) error
+}
+
+// domainValidator - バリデーション用の構造体
+type domainValidator struct {
+	validate *validator.Validate
+}
+
+// NewDomainValidator - Validatorの生成
+func NewDomainValidator() DomainValidator {
+	validate := validator.New()
+
+	if err := validate.RegisterValidation("password", passwordCheck); err != nil {
+		return nil
+	}
+
+	return &domainValidator{
+		validate: validate,
+	}
+}
+
+// Run - バリデーションの実行
+func (dv *domainValidator) Run(i interface{}) error {
+	return dv.validate.Struct(i)
+}
+
+func passwordCheck(fl validator.FieldLevel) bool {
+	return passwordRegex.MatchString(fl.Field().String())
+}
