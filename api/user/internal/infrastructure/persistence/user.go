afdaa492531fda334b2c1cad4913cf5fdb5a26d7
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-01-05 15:56:57 +0900

    feat: authenticationへ登録

--- api/user/internal/infrastructure/persistence/user.go
+++ api/user/internal/infrastructure/persistence/user.go
@@ -7,10 +7,12 @@ import (
 
 	"github.com/16francs/gran/api/user/internal/domain"
 	"github.com/16francs/gran/api/user/internal/domain/repository"
+	"github.com/16francs/gran/api/user/lib/firebase/authentication"
 	"github.com/16francs/gran/api/user/lib/firebase/firestore"
 )
 
 type userPersistence struct {
+	auth      authentication.Auth
 	firestore firestore.Firestore
 }
 
@@ -18,20 +20,28 @@ type userPersistence struct {
 const UserCollection = "users"
 
 // NewUserPersistence - UserRepositoryの生成
-func NewUserPersistence(fs firestore.Firestore) repository.UserRepository {
+func NewUserPersistence(fa authentication.Auth, fs firestore.Firestore) repository.UserRepository {
 	return &userPersistence{
+		auth:      fa,
 		firestore: fs,
 	}
 }
 
 func (r *userPersistence) Create(ctx context.Context, u *domain.User) error {
-	// TODO: addInFirebaseの実装
+	uid, err := createUserInAuth(ctx, r.auth, u)
+	if err != nil {
+		return xerrors.Errorf("Failed to UserPersistence/Create: %w", err)
+	}
 
-	if err := addInFirestore(ctx, r.firestore, u); err != nil {
+	if err = addInFirestore(ctx, r.firestore, u); err != nil {
 		return xerrors.Errorf("Failed to UserPersistence/Create: %w", err)
 	}
 
 	return nil
+}
+
+func createUserInAuth(ctx context.Context, fa authentication.Auth, u *domain.User) (string, error) {
+	return fa.CreateUser(ctx, u.Email, u.Password)
 }
 
 func addInFirestore(ctx context.Context, fs firestore.Firestore, u *domain.User) error {
