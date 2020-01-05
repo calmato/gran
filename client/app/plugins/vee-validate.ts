9ffc287de648492bd9e68968c959517693c6da10
yamaday <e155417k@st.u-gakugei.ac.jp>
2020-01-05 16:03:40 +0900

    fix: 除外するルールをcamelcaseだけに指定

--- client/app/plugins/vee-validate.ts
+++ client/app/plugins/vee-validate.ts
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable camelcase */
 import Vue from 'vue'
 import { ValidationProvider, ValidationObserver, localize, extend } from 'vee-validate'
 import ja from 'vee-validate/dist/locale/ja.json'
