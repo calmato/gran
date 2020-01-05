074a2e266876f7d3d5405ce8f1d0e71a42ca8969
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-01-05 15:21:06 +0900

    refactor: lint

--- api/user/lib/firebase/authentication/client.go
+++ api/user/lib/firebase/authentication/client.go
@@ -9,23 +9,23 @@ import (
 	"firebase.google.com/go/auth"
 )
 
-// Authentication - Authenticationの構造体
-type Authentication struct {
+// Auth - Authenticationの構造体
+type Auth struct {
 	Client *auth.Client
 }
 
 // NewClient - Firebase Authenticationに接続
-func NewClient(ctx context.Context, app *firebase.App) (*Authentication, error) {
+func NewClient(ctx context.Context, app *firebase.App) (*Auth, error) {
 	client, err := app.Auth(ctx)
 	if err != nil {
 		return nil, xerrors.Errorf("Failed to NewAuthentication: %w", err)
 	}
 
-	return &Authentication{client}, nil
+	return &Auth{client}, nil
 }
 
 // VerifyIDToken - IDトークンを確認して、デコードされたトークンからデバイスのuidを取得
-func (a *Authentication) VerifyIDToken(ctx context.Context, idToken string) (string, error) {
+func (a *Auth) VerifyIDToken(ctx context.Context, idToken string) (string, error) {
 	t, err := a.Client.VerifyIDToken(ctx, idToken)
 	if err != nil {
 		return "", err
@@ -35,7 +35,7 @@ func (a *Authentication) VerifyIDToken(ctx context.Context, idToken string) (str
 }
 
 // GetUIDByEmail - メールアドレスによるユーザーUIDの取得
-func (a *Authentication) GetUIDByEmail(ctx context.Context, email string) (string, error) {
+func (a *Auth) GetUIDByEmail(ctx context.Context, email string) (string, error) {
 	u, err := a.Client.GetUserByEmail(ctx, email)
 	if err != nil {
 		return "", err
@@ -45,7 +45,7 @@ func (a *Authentication) GetUIDByEmail(ctx context.Context, email string) (strin
 }
 
 // CreateUser - 新しいFirebase Authenticationユーザーを作成
-func (a *Authentication) CreateUser(ctx context.Context, email string, password string) (string, error) {
+func (a *Auth) CreateUser(ctx context.Context, email string, password string) (string, error) {
 	params := (&auth.UserToCreate{}).
 		Email(email).
 		EmailVerified(emailVerified(email)).
@@ -61,7 +61,7 @@ func (a *Authentication) CreateUser(ctx context.Context, email string, password 
 }
 
 // UpdateUser - 既存のユーザーのデータを変更
-func (a *Authentication) UpdateUser(ctx context.Context, uid string, email string, password string, disable bool) error {
+func (a *Auth) UpdateUser(ctx context.Context, uid string, email string, password string, disable bool) error {
 	params := (&auth.UserToUpdate{}).
 		Email(email).
 		EmailVerified(emailVerified(email)).
@@ -76,7 +76,7 @@ func (a *Authentication) UpdateUser(ctx context.Context, uid string, email strin
 }
 
 // DeleteUser - 既存のユーザーをuidで削除
-func (a *Authentication) DeleteUser(ctx context.Context, uid string) error {
+func (a *Auth) DeleteUser(ctx context.Context, uid string) error {
 	return a.Client.DeleteUser(ctx, uid)
 }
 
