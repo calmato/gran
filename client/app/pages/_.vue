5e6486a9cf7e3cbdfef2e9d1442ed489e7a79117
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-01-12 01:50:34 +0900

    feat: 404ページの作成

---
+++ client/app/pages/_.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        <v-layout column align-center>
+          <h1>404 Not Found</h1>
+          <v-img src="/error-404.png" />
+
+          <v-btn @click="handleClick">ホームへ戻る</v-btn>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  layout: 'empty',
+
+  methods: {
+    handleClick() {
+      this.$router.push('/')
+    }
+  }
+})
+</script>
