55ea3bb3c298568ce0863d0e743b87d1717aa104
yamaday <e155417k@st.u-gakugei.ac.jp>
2020-01-12 23:37:34 +0900

    fix: pathの修正
    このコンポーネントは汎用性をもたせたいな．．．
    pathとかをdataで持つか🤔

--- client/app/components/organisms/GranLinkList.vue
+++ client/app/components/organisms/GranLinkList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ma-2 text-center">
     <p>
-      <nuxt-link to="/singup">新規登録する</nuxt-link>
+      <nuxt-link to="/signup">新規登録する</nuxt-link>
     </p>
     <p>
       <nuxt-link to="/forget">パスワードを忘れた</nuxt-link>
