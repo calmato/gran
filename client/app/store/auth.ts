339b9fa6cd907cc42d7eb15e3c2f5cd42b78ba3a
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-01-12 15:55:19 +0900

    feat: Tokenの取得

--- client/app/store/auth.ts
+++ client/app/store/auth.ts
@@ -2,28 +2,58 @@ import { ILoginForm } from '../types/form/auth/login'
 import { IUserStore, IUser } from '~/types/store/auth'
 
 export const state = (): IUserStore => ({
+  emailVerified: false,
+  token: '',
   user: {} as IUser
 })
 
+export const getters = {
+  emailVerified: (state: IUserStore) => state.emailVerified,
+  token: (state: IUserStore) => state.token,
+  user: (state: IUserStore) => state.user
+}
+
 export const mutations = {
+  setEmailVerified(state: IUserStore, emailVerified: boolean) {
+    state.emailVerified = emailVerified
+  },
+
+  setToken(state: IUserStore, token: string) {
+    state.token = token
+  },
+
   setUser(state: IUserStore, user: IUser) {
     state.user = user
   }
 }
 
 export const actions = {
-  loginWithEmailAndPassword({ commit }, form: ILoginForm): Promise<void> {
+  async setIdToken({ commit }) {
+    await this.$firebase
+      .auth()
+      .currentUser.getIdToken(true)
+      .then((token: string) => {
+        commit('setToken', token)
+      })
+      .catch((err: any) => {
+        throw err
+      })
+  },
+
+  loginWithEmailAndPassword({ commit, dispatch }, form: ILoginForm): Promise<void> {
     return this.$firebase
       .auth()
       .signInWithEmailAndPassword(form.email, form.password)
-      .then((auth: any) => {
+      .then(async (auth: any) => {
         const user: IUser = {
           uid: auth.user.uid,
           email: auth.user.email,
           creationTime: auth.user.metadata.creationTime,
           lastSignInTime: auth.user.metadata.lastSignInTime
         }
+        await dispatch('setIdToken')
         commit('setUser', user)
+        commit('setEmailVerified', auth.user.emailVerified)
         return Promise.resolve()
       })
       .catch((error: any) => {
