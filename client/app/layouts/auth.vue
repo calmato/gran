7a80d953842f0f3c35d2449d9e707f3785b9ec22
yamaday <e155417k@st.u-gakugei.ac.jp>
2020-01-13 22:47:51 +0900

    fix: layputファイルを修正してセンタリング
    このプルリクとは関係ないが，layoutの修正をした

--- client/app/layouts/auth.vue
+++ client/app/layouts/auth.vue
@@ -2,12 +2,14 @@
   <v-app id="main">
     <v-content>
       <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="4">
-            <h1 class="font-weight-black white--text text-center">Gran</h1>
-            <nuxt />
-          </v-col>
-        </v-row>
+        <v-layout>
+          <v-row align="center" justify="center">
+            <v-col cols="12" sm="8" md="4">
+              <h1 class="font-weight-black white--text text-center">Gran</h1>
+              <nuxt />
+            </v-col>
+          </v-row>
+        </v-layout>
       </v-container>
     </v-content>
   </v-app>
