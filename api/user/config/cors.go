17e4b8796592b924735e04133a6553179b0fce98
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-01-15 22:06:30 +0900

    feat: Corsの設定

--- api/user/config/cors.go
+++ api/user/config/cors.go
@@ -6,18 +6,16 @@ import (
 	wrapper "github.com/rs/cors/wrapper/gin"
 )
 
+// SetCors - CORSの設定
 func SetCors() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		options := cors.Options{
-			AllowedOrigins:   []string{"*"},
-			AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-			AllowedHeaders:   []string{"*"},
-			AllowCredentials: false,
-			MaxAge:           1440, // 60m * 24h
-			Debug:            false,
-		}
-
-		wrapper.New(options)
-		ctx.Next()
+	options := cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: false,
+		MaxAge:           1440, // 60m * 24h
+		Debug:            false,
 	}
+
+	return wrapper.New(options)
 }
