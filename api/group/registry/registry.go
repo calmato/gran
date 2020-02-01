0bb3fc29a1494e9cdcbfd3055a551a85cdc4eaa3
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-02-01 17:30:06 +0900

    feat: Registry

--- api/group/registry/registry.go
+++ api/group/registry/registry.go
@@ -2,21 +2,25 @@ package registry
 
 import (
 	"github.com/16francs/gran/api/group/internal/interface/handler"
+	v1 "github.com/16francs/gran/api/group/internal/interface/handler/v1"
 	"github.com/16francs/gran/api/group/lib/firebase/authentication"
 	"github.com/16francs/gran/api/group/lib/firebase/firestore"
 )
 
 // Registry - DIコンテナ
 type Registry struct {
-	Health handler.APIHealthHandler
+	Health  handler.APIHealthHandler
+	V1Group v1.APIV1GroupHandler
 }
 
 // NewRegistry - internalディレクトリのファイルを読み込み
 func NewRegistry(fa *authentication.Auth, fs *firestore.Firestore) *Registry {
 	health := healthInjection()
+	v1Group := V1GroupInjection(fa, fs)
 
 	return &Registry{
-		Health: health,
+		Health:  health,
+		V1Group: v1Group,
 	}
 }
 
