56e8f552e185e8faf9986aa12fc8cd7615be2704
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-02-01 17:20:36 +0900

    feat: Infrastructure

---
+++ api/group/internal/infrastructure/persistence/group.go
@@ -0,0 +1,48 @@
+package persistence
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/16francs/gran/api/group/internal/domain"
+	"github.com/16francs/gran/api/group/internal/domain/repository"
+	"github.com/16francs/gran/api/group/lib/firebase/firestore"
+)
+
+type groupPersistence struct {
+	firestore *firestore.Firestore
+}
+
+// NewGroupPersistence - GroupRepositoryの生成
+func NewGroupPersistence(fs *firestore.Firestore) repository.GroupRepository {
+	return &groupPersistence{
+		firestore: fs,
+	}
+}
+
+func (gp *groupPersistence) Create(ctx context.Context, u *domain.User, g *domain.Group) error {
+	g.ID = uuid.New().String()
+	g.UserRefs = append(g.UserRefs, getUserReference(u.ID))
+
+	if err := gp.firestore.Set(ctx, GroupCollection, g.ID, g); err != nil {
+		return err
+	}
+
+	current := time.Now()
+
+	u.GroupRefs = append(u.GroupRefs, g.ID)
+	u.UpdatedAt = current
+
+	if err := gp.firestore.Set(ctx, UserCollection, u.ID, u); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getUserReference(userID string) string {
+	return strings.Join([]string{GroupCollection, userID}, "/")
+}
