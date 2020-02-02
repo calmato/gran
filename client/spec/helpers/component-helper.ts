b007972da8ad358b5778f2d7449992d0c1d3d05f
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-02-02 15:44:06 +0900

    feat: pagesのテストを実装

--- client/spec/helpers/component-helper.ts
+++ client/spec/helpers/component-helper.ts
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Vuetify from 'vuetify'
-import { ValidationProvider } from 'vee-validate'
+import { ValidationProvider, ValidationObserver } from 'vee-validate'
 
 Vue.use(Vuex)
 Vue.use(Vuetify)
 
 Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
