a5eee1c142343df64b559e47e14747a4780ac6a0
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-02-02 12:57:41 +0900

    feat: componentsディレクトリのテスト用ヘルパーの編集

--- client/spec/helpers/component-helper.ts
+++ client/spec/helpers/component-helper.ts
@@ -2,13 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import Vuetify from 'vuetify'
 import { ValidationProvider } from 'vee-validate'
-import AxiosHelper from '~~/spec/helpers/axios'
-import StoreModule from '~~/spec/helpers/store'
 
 Vue.use(Vuex)
 Vue.use(Vuetify)
 
 Vue.component('ValidationProvider', ValidationProvider)
-
-export const Axios = () => AxiosHelper
-export const Store = () => new Vuex.Store(StoreModule)
