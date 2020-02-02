8639cc627a91bb1e2d04561ed6d439f21a1abfeb
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-02-03 00:02:34 +0900

    feat: ボードRequestValidationの実装

---
+++ api/todo/internal/application/request/board.go
@@ -0,0 +1,10 @@
+package request
+
+// CreateBoard - ボード作成APIのリクエスト
+type CreateBoard struct {
+	Name            string `json:"name" validate:"required"`
+	GroupID         string `json:"groupId" validate:"required"`
+	Closed          bool   `firestore:"closed" validate:"required"`
+	ThumbnailURL    string `firestore:"thumbnailUrl"`
+	BackgroundColor string `firestore:"backgroundColor"`
+}
---
+++ api/todo/internal/application/validation/board.go
@@ -0,0 +1,28 @@
+package validation
+
+import (
+	"github.com/16francs/gran/api/todo/internal/application/request"
+	"github.com/16francs/gran/api/todo/internal/domain"
+)
+
+// BoardRequestValidation - ユーザー関連のバリデーション
+type BoardRequestValidation interface {
+	CreateBoard(cb *request.CreateBoard) []*domain.ValidationError
+}
+
+type boardRequestValidation struct {
+	validator RequestValidator
+}
+
+// NewBoardRequestValidation - BoardRequestValidationの生成
+func NewBoardRequestValidation() BoardRequestValidation {
+	rv := NewRequestValidator()
+
+	return &boardRequestValidation{
+		validator: rv,
+	}
+}
+
+func (brv *boardRequestValidation) CreateBoard(cb *request.CreateBoard) []*domain.ValidationError {
+	return grv.validator.Run(cb)
+}
