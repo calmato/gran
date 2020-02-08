a2b6aa4aaf77e42d5c078d943a8f4c09429e2765
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-02-08 21:28:26 +0900

    feat: UpdateGroup アプリケーション層の実装

--- api/group/internal/application/group.go
+++ api/group/internal/application/group.go
@@ -17,6 +17,7 @@ type GroupApplication interface {
 	Index(ctx context.Context) ([]*domain.Group, error)
 	Show(ctx context.Context, groupID string) (*domain.Group, error)
 	Create(ctx context.Context, req *request.CreateGroup) error
+	Update(ctx context.Context, groupID string, req *request.UpdateGroup) error
 }
 
 type groupApplication struct {
@@ -91,6 +92,39 @@ func (ga *groupApplication) Create(ctx context.Context, req *request.CreateGroup
 	}
 
 	if err := ga.groupService.Create(ctx, u, g); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ga *groupApplication) Update(ctx context.Context, groupID string, req *request.UpdateGroup) error {
+	u, err := ga.userService.Authentication(ctx)
+	if err != nil {
+		return err
+	}
+
+	g, err := ga.groupService.Show(ctx, groupID)
+	if err != nil {
+		return err
+	}
+
+	if !ga.groupService.ExistUserIDInUserRefs(ctx, u.ID, g) {
+		err = xerrors.New("Failed to Application")
+		return domain.Forbidden.New(err)
+	}
+
+	if ves := ga.groupRequestValidation.UpdateGroup(req); len(ves) > 0 {
+		err := xerrors.New("Failed to Application/RequestValidation")
+		return domain.InvalidRequestValidation.New(err, ves...)
+	}
+
+	current := time.Now()
+	g.Name = req.Name
+	g.Description = req.Description
+	g.UpdatedAt = current
+
+	if err := ga.groupService.Update(ctx, g); err != nil {
 		return err
 	}
 
