5f1bcdf28bc235119e297d3dc87a0493e6ee8440
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-02-08 19:52:28 +0900

    feat: validationの編集

--- api/group/internal/infrastructure/validation/base.go
+++ api/group/internal/infrastructure/validation/base.go
@@ -1,7 +1,6 @@
 package validation
 
 import (
-	"fmt"
 	"regexp"
 
 	"github.com/go-playground/validator/v10"
@@ -64,12 +63,8 @@ func passwordCheck(fl validator.FieldLevel) bool {
 	return passwordRegex.MatchString(fl.Field().String())
 }
 
-func validationMessage(tag string, param string) string {
+func validationMessage(tag string) string {
 	switch tag {
-	case validation.MinTag:
-		return fmt.Sprintf(validation.MinMessage, param)
-	case validation.MaxTag:
-		return fmt.Sprintf(validation.MaxMessage, param)
 	case validation.EmailTag:
 		return validation.EmailMessage
 	case validation.PasswordTag:
