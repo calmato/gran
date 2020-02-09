13bd73da8f27db0688c751eca674a58c34b86ef2
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-02-09 21:05:47 +0900

    feat: user_api エラーメッセージを日本語に

--- api/user/internal/application/request/user.go
+++ api/user/internal/application/request/user.go
@@ -2,7 +2,7 @@ package request
 
 // CreateUser - ユーザー登録APIのリクエスト
 type CreateUser struct {
-	Email                string `json:"email" validate:"required,email,max=256"`
-	Password             string `json:"password" validate:"required,password,min=6,max=32"`
-	PasswordConfirmation string `json:"passwordConfirmation" validate:"required,eqfield=Password"`
+	Email                string `json:"email" label:"メールアドレス" validate:"required,email,max=256"`
+	Password             string `json:"password" label:"パスワード" validate:"required,password,min=6,max=32"`
+	PasswordConfirmation string `json:"passwordConfirmation" label:"パスワード(確認用)" validate:"required,eqfield=Password"`
 }
