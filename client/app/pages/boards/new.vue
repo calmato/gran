f758bfda454a40a3f80b8bdf8089bfe8ef374d69
yamaday <e155417k@st.u-gakugei.ac.jp>
2020-02-22 23:02:07 +0900

    fix: スペルミスを修正

--- client/app/pages/boards/new.vue
+++ client/app/pages/boards/new.vue
@@ -1,21 +1,18 @@
 <template>
   <div>
     <gran-board-new />
-    <gran-avatar />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import GranBoardNew from '~/components/templates/GranBoardNew.vue'
-import GranAvatar from '~/components/atoms/GranAvatar.vue'
 
 export default Vue.extend({
-  layout: 'header',
+  layout: 'home',
 
   components: {
-    GranBoardNew,
-    GranAvatar
+    GranBoardNew
   }
 })
 </script>
