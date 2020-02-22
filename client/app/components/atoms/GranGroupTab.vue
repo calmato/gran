a227117f2d546400379e14aa667b61741ceeef6d
yamaday <e155417k@st.u-gakugei.ac.jp>
2020-02-22 23:01:36 +0900

    feat: tabメニューをヘッダー直下に配置

---
+++ client/app/components/atoms/GranGroupTab.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-tabs centered fixed-tabs background-color="#039be5" dark>
+    <v-tab v-for="(tab, index) in tabs" :key="index">
+      {{ tab }}
+    </v-tab>
+  </v-tabs>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    tabs: {
+      type: Array,
+      default: () => {
+        return ['グループ', 'ボード']
+      }
+    }
+  }
+})
+</script>
