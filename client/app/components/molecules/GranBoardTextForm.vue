94a43795f161abb235b999e830e8101721a2a89b
yamaday <e155417k@st.u-gakugei.ac.jp>
2020-02-22 23:07:23 +0900

    [wip] feat: borad formの作成

---
+++ client/app/components/molecules/GranBoardTextForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <gran-form-group>
+      <gran-text-field />
+      <gran-text-field />
+      <gran-text-field />
+      <gran-text-field />
+    </gran-form-group>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import GranTextField from '~/components/atoms/GranTextField.vue'
+import GranFormGroup from '~/components/atoms/GranFormGroup.vue'
+
+export default Vue.extend({
+  components: {
+    GranFormGroup,
+    GranTextField
+  }
+})
+</script>
