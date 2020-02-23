ed81607f2eb02f1b716d119a32ea6d03b85d8277
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-02-24 02:25:04 +0900

    feat: diagramsを導入してみる

--- doc/03_infrastructure/01_design/diagrams/gcp_design.py
+++ doc/03_infrastructure/01_design/diagrams/gcp_design.py
@@ -4,8 +4,9 @@ from diagrams.gcp.network import CDN, DNS, LoadBalancing
 from diagrams.gcp.storage import GCS
 from diagrams.gcp.compute import GKE
 from diagrams.onprem.compute import Server
+from diagrams.oci.network import Internetgateway
 
-with Diagram("Gran - GCP Desigin", show=False):
+with Diagram("Gran - GCP Design", show=False):
 	server = Server("Client (Nuxt.js)")
 
 	with Cluster("Google Cloud Platform"):
@@ -15,8 +16,10 @@ with Diagram("Gran - GCP Desigin", show=False):
 		lb = LoadBalancing("Cloud Load Balancing")
 		gcs = GCS("Cloud Storage")
 		gke = GKE("Kubernetes Engine")
+		net = Internetgateway("Gateway")
 
 		dns >> cdn >> gcs
 		dns >> lb >> gke >> firestore
 
-	server >> dns
+	server >> net
+	net >> dns
