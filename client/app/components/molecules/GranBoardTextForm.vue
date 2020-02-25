3a91fab97f9825fcc5011260e8bb7850f2ed4f76
yamaday <e155417k@st.u-gakugei.ac.jp>
2020-02-25 14:34:48 +0900

    [wip] 作業中のをコミットする

--- client/app/components/molecules/GranBoardTextForm.vue
+++ client/app/components/molecules/GranBoardTextForm.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <gran-form-group>
-      <gran-text-field />
-      <gran-text-field />
-      <gran-text-field />
-      <gran-text-field />
+      <gran-text-field label="ボード名" />
+      <gran-select label="グループ" />
+      <gran-select label="公開範囲" />
+      <gran-select label="背景" />
+      <v-color-picker v-model="color" />
     </gran-form-group>
   </div>
 </template>
@@ -12,12 +13,20 @@
 <script lang="ts">
 import Vue from 'vue'
 import GranTextField from '~/components/atoms/GranTextField.vue'
+import GranSelect from '~/components/atoms/GranSelect.vue'
 import GranFormGroup from '~/components/atoms/GranFormGroup.vue'
 
 export default Vue.extend({
   components: {
     GranFormGroup,
+    GranSelect,
     GranTextField
+  },
+  props: {},
+  data: () => {
+    return {
+      color: ''
+    }
   }
 })
 </script>
