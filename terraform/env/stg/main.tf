e5c59cd801226880a8dd5a5ebe3c0962db407721
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-02-25 22:07:44 +0900

    feat: ステージング環境の設定

--- terraform/env/stg/main.tf
+++ terraform/env/stg/main.tf
@@ -1,7 +1,7 @@
 provider "google" {
   project     = var.project_id
   region      = "asia-northeast1"
-  credentials = file("account.json")
+  credentials = file(var.credential_file)
 }
 
 module "this" {
@@ -9,6 +9,24 @@ module "this" {
 
   location = "asia-northeast1-a"
 
-  gke_cluster_name        = "gran_master"
-  gke_cluster_description = "Gran GKE Master"
+  #################################################
+  # GKE Cluster
+  #################################################
+  gke_cluster_name        = "gran_cluster"
+  gke_cluster_description = "gran application cluster for staging"
+
+  gke_cluster_min_master_version = "1.14.10-gke.17"
+
+  #################################################
+  # GKE Node
+  #################################################
+  gke_node_name_prefix = "gran_node"
+
+  gke_node_count        = 1
+  gke_node_machine_type = "f1-micro"
+
+  tags = {
+    service = "gran"
+    env     = "stg"
+  }
 }
