ee281c7126985d5fe074896553ff6cf469a0ccf2
濵田広大 <hamany0326@gmail.com>
2020-03-03 01:00:32 +0900

    add: ボード一覧の説明の追加

--- client/app/components/molecules/GranGroupNewForm.vue
+++ client/app/components/molecules/GranGroupNewForm.vue
@@ -16,7 +16,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { GroupNewForm, IGroupForm, GroupNewFormValidate } from '~/types'
+import { GroupNewForm, IGroupForm, GroupNewFormValidate } from '~/types/form'
 import GranTextField from '~/components/atoms/GranTextField.vue'
 import GranTextArea from '~/components/atoms/GranTextArea.vue'
 import GranButton from '~/components/atoms/GranButton.vue'
--- client/app/components/organisms/GranGroupList.vue
+++ client/app/components/organisms/GranGroupList.vue
@@ -1,28 +1,42 @@
 <template>
-  <v-card max-width="max" class="mx-auto">
-    <v-list three-line>
-      <template v-for="(group, index) in groups">
-        <v-list-item :key="index">
-          <v-list-item-avatar>
-            <v-img :src="group.avatar"></v-img>
-          </v-list-item-avatar>
-          <v-list-item-content>
-            <v-list-item-title>{{ group.name }}</v-list-item-title>
-            <v-list-item-subtitle>{{ group.description }}</v-list-item-subtitle>
-            <v-divider :key="index"> </v-divider>
-          </v-list-item-content>
-        </v-list-item>
-      </template>
-    </v-list>
-  </v-card>
+  <div>
+    <v-row>
+      <gran-icon :x-large="xLarge" name="account" />
+      <p class="ma-3 pt-1">参加中のボード一覧</p>
+    </v-row>
+    <v-card max-width="max" class="mx-auto">
+      <v-list three-line>
+        <template v-for="(group, index) in groups">
+          <v-list-item :key="index">
+            <v-list-item-avatar>
+              <v-img :src="group.avatar"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>{{ group.name }}</v-list-item-title>
+              <v-list-item-subtitle>{{ group.description }}</v-list-item-subtitle>
+              <v-divider :key="index"> </v-divider>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-card>
+  </div>
 </template>
 
 <script lang="ts">
+import Vue from 'vue'
 import { mapGetters } from 'vuex'
+import GranIcon from '~/components/atoms/GranIcon.vue'
 
-export default {
+export default Vue.extend({
+  components: {
+    GranIcon
+  },
   computed: {
     ...mapGetters('group', ['groups'])
-  }
-}
+  },
+  data: () => ({
+    xLarge: true
+  })
+})
 </script>
