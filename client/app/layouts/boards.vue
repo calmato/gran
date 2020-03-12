91397d6b319ad120b94a32ba2e94aeb1f2d3480d
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-03-12 22:47:17 +0900

    feat: boardsのlayoutファイルを作成

---
+++ client/app/layouts/boards.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar color="grey darken-1" dark app>
+      <v-app-bar-nav-icon @click="back">
+        <gran-icon name="chevron-left" />
+      </v-app-bar-nav-icon>
+      <v-toolbar-title>ボード名</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container color="grey lighten-5">
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import GranIcon from '~/components/atoms/GranIcon.vue'
+
+export default Vue.extend({
+  components: {
+    GranIcon
+  },
+  methods: {
+    back() {
+      this.$router.go(-1)
+    }
+  }
+})
+</script>
