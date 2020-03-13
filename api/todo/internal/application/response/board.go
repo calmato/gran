cf68f80cce2e59405ad54f3d0311ede92e2328e5
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-03-13 23:51:03 +0900

    feat: create show board response

--- api/todo/internal/application/response/board.go
+++ api/todo/internal/application/response/board.go
@@ -19,3 +19,28 @@ type Board struct {
 type Boards struct {
 	Boards []*Board `json:"boards"`
 }
+
+// ShowBoard - ボード詳細のレスポンス
+type ShowBoard struct {
+	ID              string   `json:"id"`
+	Name            string   `json:"name"`
+	Closed          bool     `json:"closed"`
+	ThumbnailURL    string   `json:"thumbnailUrl"`
+	BackgroundColor string   `json:"backgroundColor"`
+	Labels          []string `json:"labels"`
+	GroupID         string   `json:"groupId"`
+	Lists           []struct {
+		ID    string `json:"id"`
+		Name  string `json:"name"`
+		Color string `json:"color"`
+		Tasks []struct {
+			ID              string    `json:"id"`
+			Name            string    `json:"name"`
+			Labels          []string  `json:"labels"`
+			AssignedUserIDs []string  `json:"assignedUserIds"`
+			DeadlinedAt     time.Time `json:"deadlinedAt"`
+		} `json:"tasks"`
+	} `json:"lists"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
