788f4ba16ea7c72da7d31866f79df4f479539f3e
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-03-14 00:51:15 +0900

    feat: add group repository methods

--- api/todo/internal/domain/repository/board.go
+++ api/todo/internal/domain/repository/board.go
@@ -9,5 +9,7 @@ import (
 // BoardRepository - BoardRepositoryインターフェース
 type BoardRepository interface {
 	Index(ctx context.Context, groupID string) ([]*domain.Board, error)
+	Show(ctx context.Context, groupID string, boardID string) (*domain.Board, error)
 	Create(ctx context.Context, b *domain.Board) error
+	IndexBoardList(ctx context.Context, groupID string, boardID string) ([]*domain.BoardList, error)
 }
--- api/todo/internal/infrastructure/persistence/collection.go
+++ api/todo/internal/infrastructure/persistence/collection.go
@@ -9,6 +9,8 @@ const (
 	GroupCollection = "groups"
 	// BoardCollection - BoardCollection名
 	BoardCollection = "boards"
+	// BoardListCollection - BoardListCollection名
+	BoardListCollection = "board_lists"
 )
 
 func GetGroupReference(groupID string) string {
@@ -17,4 +19,8 @@ func GetGroupReference(groupID string) string {
 
 func GetBoardCollection(groupID string) string {
 	return strings.Join([]string{GroupCollection, groupID, BoardCollection}, "/")
+}
+
+func GetBoardListCollection(groupID string, boardID string) string {
+	return strings.Join([]string{GroupCollection, groupID, BoardCollection, boardID, BoardListCollection}, "/")
 }
