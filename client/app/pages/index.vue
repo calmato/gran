e758d4f479fca0c3ab9b0c6cdde93c9641c94c8b
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-03-13 14:57:56 +0900

    feat: 各ボードへの導線として仮置き

--- client/app/pages/index.vue
+++ client/app/pages/index.vue
@@ -1,52 +1,25 @@
 <template>
   <v-layout column justify-center align-center>
-    <hr />
-    <p>タスクカードコンポーネント</p>
-    <v-window :show-arrows="showArrows" reverse>
-      <v-window-item v-for="(list, i) in lists" :key="i">
-        <draggable :list="list.array" class="dragArea list-group">
-          <transition-group>
-            <div v-for="(element, index) in list.array" :key="index" class="pa-1 mb-4">
-              <v-card>
-                <div class="text-left pa-1 my-1">
-                  <v-chip class="px-2" x-small>Default</v-chip>
-                  <v-chip class="px-2" color="primary" x-small>Default</v-chip>
-                  <v-chip class="px-2" color="secondary" x-small>Default</v-chip>
-                </div>
-                <v-card-title class="headline px-4 pt-0">
-                  {{ element }}
-                </v-card-title>
-                <v-card-text>
-                  <div class="text-left my-2"></div>
-                </v-card-text>
-              </v-card>
-            </div>
-          </transition-group>
-        </draggable>
-      </v-window-item>
-    </v-window>
+    <h1>今日の予定</h1>
+    <v-divider />
+    <gran-task-card @click="click" />
   </v-layout>
 </template>
 
 <script lang="ts">
-import draggable from 'vuedraggable'
 import Vue from 'vue'
+import GranTaskCard from '~/components/molecules/GranTaskCard.vue'
 
 export default Vue.extend({
   layout: 'home',
   components: {
-    draggable
+    GranTaskCard
   },
-  data: () => ({
-    lists: [
-      {
-        array: ['UI設計考える', 'タスク', 'タスク2']
-      },
-      {
-        array: ['UI設計考える', 'タスク3', 'タスク4']
-      }
-    ],
-    showArrows: true
-  })
+  data: () => ({}),
+  methods: {
+    click() {
+      this.$router.push('/boards/1')
+    }
+  }
 })
 </script>
