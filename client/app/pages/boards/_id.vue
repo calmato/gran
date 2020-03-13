75f46870af8b618cc02f6696282ba2147880dcae
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-03-13 20:42:09 +0900

    refactor: templatesのコンポーネントを呼び出すように変更

--- client/app/pages/boards/_id.vue
+++ client/app/pages/boards/_id.vue
@@ -1,27 +1,15 @@
 <template>
-  <v-layout col>
-    <gran-kanban-card v-for="(list, index) in lists" :key="index">
-      <draggable :list="list.array" class="dragArea list-group">
-        <div v-for="(element, i) in list.array" :key="i" class="pa-1 mb-4">
-          <gran-task-card />
-        </div>
-      </draggable>
-    </gran-kanban-card>
-  </v-layout>
+  <gran-kanban />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import draggable from 'vuedraggable'
-import GranTaskCard from '~/components/molecules/GranTaskCard.vue'
-import GranKanbanCard from '~/components/molecules/GranKanbanCard.vue'
+import GranKanban from '~/components/templates/GranKanban.vue'
 
 export default Vue.extend({
   layout: 'boards',
   components: {
-    draggable,
-    GranTaskCard,
-    GranKanbanCard
+    GranKanban
   },
   data: () => ({
     lists: [
