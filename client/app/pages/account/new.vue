aa7e0e2c76031aee3d5d9ca24f805c4ed79d5baa
濵田広大 <hamany0326@gmail.com>
2020-03-18 19:58:15 +0900

    アカウント編集画面の実装

---
+++ client/app/pages/account/new.vue
@@ -0,0 +1,16 @@
+<template>
+  <gran-account-new />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import GranAccountNew from '~/components/templates/GranAccountNew.vue'
+
+export default Vue.extend({
+  layout: 'home',
+
+  components: {
+    GranAccountNew
+  }
+})
+</script>
---
+++ client/app/types/form/auth/account.ts
@@ -0,0 +1,46 @@
+import { IStringForm } from '../utils/form'
+
+export interface IAccountForm {
+  name: IStringForm
+  userName: IStringForm
+  profile: IStringForm
+  email: IStringForm
+  phone: IStringForm
+}
+
+interface IAccountFormValidate {
+  name: Object
+  userName: Object
+}
+
+export const AccountForm: IAccountForm = {
+  name: {
+    label: 'Name',
+    value: ''
+  },
+  userName: {
+    label: 'UserName',
+    value: ''
+  },
+  profile: {
+    label: 'Bio',
+    value: ''
+  },
+  email: {
+    label: 'Email',
+    value: ''
+  },
+  phone: {
+    label: 'Phone',
+    value: ''
+  }
+}
+
+export const AccountFormValidate: IAccountFormValidate = {
+  name: {
+    required: true
+  },
+  userName: {
+    required: true
+  }
+}
