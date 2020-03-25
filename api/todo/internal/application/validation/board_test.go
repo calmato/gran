140afa92c3f5e2560162b88eb39f06941b389f3d
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-03-25 22:42:26 +0900

    refactor: test for todo_api

--- api/todo/internal/application/validation/board_test.go
+++ api/todo/internal/application/validation/board_test.go
@@ -3,6 +3,7 @@ package validation
 import (
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/16francs/gran/api/todo/internal/application/request"
 	"github.com/16francs/gran/api/todo/internal/domain"
@@ -59,6 +60,56 @@ func TestBoardRequestValidation_CreateBoardList(t *testing.T) {
 			target := NewBoardRequestValidation()
 
 			got := target.CreateBoardList(testCase.Request)
+			if !reflect.DeepEqual(got, testCase.Expected) {
+				t.Fatalf("want %#v, but %#v", testCase.Expected, got)
+				return
+			}
+		})
+	}
+}
+
+func TestBoardRequestValidation_UpdateKanban(t *testing.T) {
+	current := time.Now()
+
+	testCases := map[string]struct {
+		Request  *request.UpdateKanban
+		Expected []*domain.ValidationError
+	}{
+		"ok": {
+			Request: &request.UpdateKanban{
+				Lists: []*request.ListInUpdateKanban{
+					{
+						ID:    "board-list-id-01",
+						Name:  "テストボードリスト01",
+						Color: "red",
+						Tasks: []*request.TaskInUpdateKanban{
+							{
+								ID:              "task-id",
+								Name:            "テストタスク",
+								Labels:          make([]string, 0),
+								AssignedUserIDs: make([]string, 0),
+								DeadlinedAt:     current,
+							},
+						},
+					},
+					{
+						ID:    "board-list-id-02",
+						Name:  "テストボードリスト02",
+						Color: "red",
+						Tasks: make([]*request.TaskInUpdateKanban, 0),
+					},
+				},
+			},
+			Expected: make([]*domain.ValidationError, 0),
+		},
+	}
+
+	for result, testCase := range testCases {
+		// Start test
+		t.Run(result, func(t *testing.T) {
+			target := NewBoardRequestValidation()
+
+			got := target.UpdateKanban(testCase.Request)
 			if !reflect.DeepEqual(got, testCase.Expected) {
 				t.Fatalf("want %#v, but %#v", testCase.Expected, got)
 				return
