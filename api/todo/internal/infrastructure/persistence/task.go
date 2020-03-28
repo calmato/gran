a8dce9c9e9a3ebc660947c968e9de7e248a6d1dd
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-03-28 18:49:19 +0900

    fix: test

--- api/todo/internal/infrastructure/persistence/task.go
+++ api/todo/internal/infrastructure/persistence/task.go
@@ -21,41 +21,6 @@ func NewTaskPersistence(fs *firestore.Firestore) repository.TaskRepository {
 	}
 }
 
-// TODO: remove
-func (tp *taskPersistence) IndexByBoardListID(ctx context.Context, boardListID string) ([]*domain.Task, error) {
-	ts := make([]*domain.Task, 0)
-	taskCollection := GetTaskCollection()
-
-	q := &firestore.Query{
-		Field:    "board_list_id", // TODO: Taskエンティティのタグから取得
-		Operator: "==",
-		Value:    boardListID,
-	}
-
-	iter := tp.firestore.GetByQuery(ctx, taskCollection, q)
-	for {
-		doc, err := iter.Next()
-		if err == iterator.Done {
-			break
-		}
-
-		if err != nil {
-			return nil, err
-		}
-
-		t := &domain.Task{}
-
-		err = doc.DataTo(t)
-		if err != nil {
-			return nil, err
-		}
-
-		ts = append(ts, t)
-	}
-
-	return ts, nil
-}
-
 func (tp *taskPersistence) IndexByBoardID(ctx context.Context, boardID string) ([]*domain.Task, error) {
 	ts := make([]*domain.Task, 0)
 	taskCollection := GetTaskCollection()
