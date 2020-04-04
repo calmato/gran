977ba5d84510128c10cfa30c89d2e175398c9a28
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-04-04 16:42:56 +0900

    feat: Update user domain

--- api/user/internal/application/request/user.go
+++ api/user/internal/application/request/user.go
@@ -6,3 +6,14 @@ type CreateUser struct {
 	Password             string `json:"password" label:"パスワード" validate:"required,password,min=6,max=32"`
 	PasswordConfirmation string `json:"passwordConfirmation" label:"パスワード(確認用)" validate:"required,eqfield=Password"`
 }
+
+// UpdateUser - ユーザー編集APIのリクエスト
+// TODO: Add Password, PhoneNumber(regex)
+type UpdateUser struct {
+	Name        string `json:"name" label:"氏名" validate:"required,max=32"`
+	DisplayName string `json:"displayName" label:"表示名" validate:"required,max=32"`
+	Email       string `json:"email" label:"メールアドレス" validate:"required,email,max=256"`
+	PhoneNumber string `json:"phoneNumber" label:"電話番号" validate:"max=12"`
+	Thumbnail   string `json:"thumbnail" label:"サムネイル"`
+	Biography   string `json:"biography" label:"自己紹介" validate:"max=256"`
+}
