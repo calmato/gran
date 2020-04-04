2ec2aedb713859b06ba77691dec4ebe45d478f95
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-04-04 19:17:55 +0900

    feat: create test codes

--- api/user/internal/application/validation/user_test.go
+++ api/user/internal/application/validation/user_test.go
@@ -36,3 +36,35 @@ func TestUserRequestValidation_CreateUser(t *testing.T) {
 		})
 	}
 }
+
+func TestUserRequestValidation_UpdateProfile(t *testing.T) {
+	testCases := map[string]struct {
+		Request  *request.UpdateProfile
+		Expected []*domain.ValidationError
+	}{
+		"ok": {
+			Request: &request.UpdateProfile{
+				Name:        "テストユーザー",
+				DisplayName: "ユーザー",
+				Email:       "hoge@hoge.com",
+				PhoneNumber: "",
+				Thumbnail:   "",
+				Biography:   "",
+			},
+			Expected: make([]*domain.ValidationError, 0),
+		},
+	}
+
+	for result, testCase := range testCases {
+		// Start test
+		t.Run(result, func(t *testing.T) {
+			target := NewUserRequestValidation()
+
+			got := target.UpdateProfile(testCase.Request)
+			if !reflect.DeepEqual(got, testCase.Expected) {
+				t.Fatalf("want %#v, but %#v", testCase.Expected, got)
+				return
+			}
+		})
+	}
+}
