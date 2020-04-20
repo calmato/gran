dff09d11d2f7c55b86b8951c9316356800f5d253
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-04-20 23:18:57 +0900

    feat: グループごとのボードの一覧をlist形式でみるコンポーネントを作成した

---
+++ client/app/components/molecules/GranBoardsList.vue
@@ -0,0 +1,57 @@
+<template>
+  <gran-list>
+    <gran-subheader>
+      <gran-icon name="account" x-large />
+      {{ name }}
+    </gran-subheader>
+    <div v-for="(board, index) in boards" :key="board.id">
+      <gran-list-item color="#ffffff" exact @click="click(board.id)">
+        <gran-list-item-content>
+          {{ board.name }}
+        </gran-list-item-content>
+      </gran-list-item>
+      <v-divider v-if="index === boards.length" />
+    </div>
+  </gran-list>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import GranList from '~/components/atoms/GranList.vue'
+import GranListItem from '~/components/atoms/GranListItem.vue'
+import GranListItemContent from '~/components/atoms/GranListItemContent.vue'
+import GranSubheader from '~/components/atoms/GranSubheader.vue'
+import GranIcon from '~/components/atoms/GranIcon.vue'
+
+export default Vue.extend({
+  components: {
+    GranIcon,
+    GranList,
+    GranListItem,
+    GranListItemContent,
+    GranSubheader,
+  },
+  props: {
+    name: {
+      type: String,
+      default: 'パーソナルボード',
+    },
+    boards: {
+      type: Array,
+      default: () => {
+        return [
+          {
+            id: 'abcd',
+            name: 'テスト',
+          },
+        ]
+      },
+    },
+  },
+  methods: {
+    click(boardId: string): void {
+      this.$emit('click', boardId)
+    },
+  },
+})
+</script>
