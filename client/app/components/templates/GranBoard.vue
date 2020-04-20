b7c5742f49c988e51a37cedea35c446862269b2c
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-04-20 23:30:32 +0900

    feat: ボードへの遷移の流れを実装

--- client/app/components/templates/GranBoard.vue
+++ client/app/components/templates/GranBoard.vue
@@ -1,14 +1,24 @@
 <template>
-  <gran-board-search />
+  <div>
+    <gran-board-search />
+    <gran-boards-list @click="transition" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import GranBoardsList from '~/components/molecules/GranBoardsList.vue'
 import GranBoardSearch from '~/components/molecules/GranBoardSearch.vue'
 
 export default Vue.extend({
   components: {
     GranBoardSearch,
+    GranBoardsList,
+  },
+  methods: {
+    transition(boardId: string): void {
+      this.$router.push(`/boards/${boardId}`)
+    },
   },
 })
 </script>
