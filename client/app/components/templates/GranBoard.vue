cd1ffccb533d93e603818a9cf5f35b877847acf1
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-04-30 17:55:45 +0900

    feat: ページの遷移などを修正

--- client/app/components/templates/GranBoard.vue
+++ client/app/components/templates/GranBoard.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <gran-board-search />
-    <gran-boards-list @click="transition" />
+    <gran-boards-list @click="transitionBoardPage" @handleAddButton="transitionNewBoardPage" />
+    <gran-boards-list name="グループ" @click="transitionBoardPage" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { mapGetters } from 'vuex'
 import GranBoardsList from '~/components/molecules/GranBoardsList.vue'
 import GranBoardSearch from '~/components/molecules/GranBoardSearch.vue'
 
@@ -15,9 +17,16 @@ export default Vue.extend({
     GranBoardSearch,
     GranBoardsList,
   },
+  computed: {
+    ...mapGetters('group', ['groups']),
+  },
   methods: {
-    transition(boardId: string): void {
+    transitionBoardPage(boardId: string): void {
       this.$router.push(`/boards/${boardId}`)
+    },
+    transitionNewBoardPage(_groupId: string): void {
+      // todo: groupIDは新規作成の時に使えそうなのでコード上は書いておく
+      this.$router.push('/boards/new')
     },
   },
 })
