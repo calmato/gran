d4e820fbe320660b01f88bdc4feb729542f1344c
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-05-02 02:22:38 +0900

    refactor: index group response

--- api/todo/internal/application/response/group.go
+++ api/todo/internal/application/response/group.go
@@ -14,7 +14,25 @@ type Group struct {
 	UpdatedAt     time.Time `firestore:"updatedAt"`
 }
 
-// Groups - グループ一覧のレスポンス
-type Groups struct {
-	Groups []*Group `json:"groups"`
+// BoardsInIndexGroup - グループ一覧用 ボード一覧レスポンス
+type BoardsInIndexGroup struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// GroupInIndexGroup - グループ一覧用 グループのレスポンス
+type GroupInIndexGroup struct {
+	ID            string                `json:"id"`
+	Name          string                `json:"name"`
+	UserIDs       []string              `json:"userIds"`
+	Boards        []*BoardsInIndexGroup `json:"boards"`
+	InvitedEmails []string              `json:"invitedEmails"`
+	Description   string                `json:"description"`
+	CreatedAt     time.Time             `firestore:"createdAt"`
+	UpdatedAt     time.Time             `firestore:"updatedAt"`
+}
+
+// IndexGroups - グループ一覧のレスポンス
+type IndexGroups struct {
+	Groups []*GroupInIndexGroup `json:"groups"`
 }
