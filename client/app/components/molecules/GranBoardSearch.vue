293117a12094913525b878112355770e180eb18c
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-01 18:20:51 +0900

    feat: 検索フォームのUIを改善
    atoms/GranTextFieldを使う予定だったがvalidationの設定があると色が変化してしまうのでv-text-fieldを直接使用する🙇‍♂️

--- client/app/components/molecules/GranBoardSearch.vue
+++ client/app/components/molecules/GranBoardSearch.vue
@@ -1,17 +1,6 @@
 <template>
   <div>
-    <v-card class="mx-auto" max-width="max">
-      <v-sheet class="pa-4 lighten-2">
-        <v-text-field
-          label="Search for a board"
-          flat
-          background-color="white"
-          color="black"
-          clearable
-          clear-icon="mdi-close-circle-outline"
-        ></v-text-field>
-      </v-sheet>
-    </v-card>
+    <v-text-field label="Search for a board" prepend-inner-icon="mdi-magnify" solo flat clearable />
   </div>
 </template>
 
